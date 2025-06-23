5928e72c6ce5afbdc29b95015625ce3a77dc060d
CadePowell3 <cadepowell1022@gmail.com>
2025-06-22 22:19:37 -0500

    Update Rotate.c
    changed rotate_right to rotateRight to match name from main and header files

--- Rotate.c
+++ Rotate.c
@@ -1,6 +1,6 @@
 #include "major1.h"
 
-unsigned int rotate_right(unsigned int value, unsigned int positions) {
+unsigned int rotateRight(unsigned int value, unsigned int positions) {
     positions %= 32; 
 
     return (value >> positions) | (value << (32 - positions));
